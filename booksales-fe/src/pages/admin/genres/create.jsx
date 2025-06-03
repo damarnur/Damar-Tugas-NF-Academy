12892d285b4c9fb7892d59ff120ca4e077823c9c
damarnur <yourname1054@gmail.com>
2025-06-03 20:27:47 +0700

    revision for my first task

--- booksales-fe/src/pages/admin/genres/create.jsx
+++ booksales-fe/src/pages/admin/genres/create.jsx
@@ -38,7 +38,7 @@ export default function GenreCreate() {
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
-        <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
+        <div className="max-w-2xl px-4 py-8 mx-auto lg:py-28">
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
             Create New Genre
           </h2>
